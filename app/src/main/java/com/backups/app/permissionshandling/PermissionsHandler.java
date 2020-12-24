941f6ef56955303d304c8e7b10d83c2381971952
flyingsl0ths <randomemail32110@gmail.com>
2020-12-24 02:02:57 -0600

    Added class needed to handle permissions

---
+++ app/src/main/java/com/backups/app/permissionshandling/PermissionsHandler.java
@@ -0,0 +1,25 @@
+package com.backups.app.permissionshandling;
+
+
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.backups.app.filehandling.APKFileOperations;
+
+public class PermissionsHandler {
+    protected static boolean checkExternalFilesystemPermissions() {
+        return false;
+    }
+
+    protected static void promptUserForPermissions() {
+
+    }
+
+    protected static void askForExternalFilesystemPermissions(AppCompatActivity activity) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        activity.startActivityForResult(intent, APKFileOperations.CREATE_DIRECTORY_CODE);
+
+    }
+}
