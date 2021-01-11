b37eba2a12655fbf3dc153614a7a5d2ed66171ab
flyingsl0ths <randomemail32110@gmail.com>
2021-01-11 01:52:50 -0600

    Implemented ApkListViewModel

---
+++ app/src/main/java/com/backups/app/data/ApkListViewModel.java
@@ -0,0 +1,41 @@
+package com.backups.app.data;
+
+import android.content.pm.PackageManager;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+public class ApkListViewModel extends ViewModel {
+    private final APKFileRepository mAPKFileRepository;
+    private final PackageManager mPackageManager;
+    private boolean mHasReceivedApbList = false;
+    private final MutableLiveData<List<APKFile>> mAppListMutableLiveData;
+
+
+    public ApkListViewModel(PackageManager packageManager, APKFileRepository apkFileRepository) {
+        mAPKFileRepository = apkFileRepository;
+        mPackageManager = packageManager;
+        mAppListMutableLiveData = new MutableLiveData<>();
+        receiveInstalledApps();
+    }
+
+    private void receiveInstalledApps() {
+        mAPKFileRepository.deliverInstalledApps(mPackageManager, mAppListMutableLiveData::postValue);
+    }
+
+
+    public boolean hasRecievedApkList() {
+        return mHasReceivedApbList;
+    }
+
+    public void recievedApkList(boolean choice) {
+        mHasReceivedApbList = choice;
+    }
+
+    public LiveData<List<APKFile>> getApkData() {
+        return mAppListMutableLiveData;
+    }
+}
