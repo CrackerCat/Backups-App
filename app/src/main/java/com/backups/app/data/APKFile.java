22ab2b99fa4eaa19c8ed9d068cd99193e70a2a11
flyingsl0ths <randomemail32110@gmail.com>
2021-01-17 01:16:06 -0600

    Renamed fields

--- app/src/main/java/com/backups/app/data/APKFile.java
+++ app/src/main/java/com/backups/app/data/APKFile.java
@@ -3,48 +3,48 @@ package com.backups.app.data;
 import android.graphics.drawable.Drawable;
 
 public class APKFile {
-    private final String m_Name;
-    private final String m_PackageName;
-    private final String m_PackagePath;
-    private final long m_AppSize;
-    private final Drawable m_Icon;
+    private final String mName;
+    private final String mPackageName;
+    private final String mPackagePath;
+    private final long mAppSize;
+    private final Drawable mIcon;
 
     APKFile(final String name, final String packageName, final String packagePath, final long appSize, final Drawable icon) {
-        m_Name = name;
-        m_PackageName = packageName;
-        m_PackagePath = packagePath;
-        m_AppSize = appSize;
-        m_Icon = icon;
+        mName = name;
+        mPackageName = packageName;
+        mPackagePath = packagePath;
+        mAppSize = appSize;
+        mIcon = icon;
     }
 
     public String getName() {
-        return m_Name;
+        return mName;
     }
 
     public String getPackageName() {
-        return m_PackageName;
+        return mPackageName;
     }
 
     public String getPackagePath() {
-        return m_PackagePath;
+        return mPackagePath;
     }
 
     public long getAppSize() {
-        return m_AppSize;
+        return mAppSize;
     }
 
     public Drawable getIcon() {
-        return m_Icon;
+        return mIcon;
     }
 
     @Override
     public String toString() {
         return "ApkFile {\n" +
-                " name: " + m_Name +
-                ",\n packageName: " + m_PackageName +
-                ",\n packagePath: " + m_PackagePath +
-                ",\n appSize: " + m_AppSize +
-                ",\n icon: " + m_Icon +
+                " name: " + mName +
+                ",\n packageName: " + mPackageName +
+                ",\n packagePath: " + mPackagePath +
+                ",\n appSize: " + mAppSize +
+                ",\n icon: " + mIcon +
                 "\n}";
     }
 }
