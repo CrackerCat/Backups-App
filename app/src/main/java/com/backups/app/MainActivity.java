1c10ff10d26dedaeb4c16932b60c6b0d9b7a515a
flyingsl0ths <randomemail32110@gmail.com>
2021-01-28 02:40:18 -0600

    Added changes to adapt to refactored TabLayout class

--- app/src/main/java/com/backups/app/MainActivity.java
+++ app/src/main/java/com/backups/app/MainActivity.java
@@ -12,11 +12,13 @@ import android.os.Bundle;
 import com.backups.app.data.APKFileRepository;
 import com.backups.app.permissionshandler.PermissionsHandler;
 import com.backups.app.ui.adapters.TabAdapter;
+import com.backups.app.ui.fragments.AppListFragment;
+import com.backups.app.ui.fragments.AppQueueFragment;
+import com.backups.app.ui.fragments.SettingsFragment;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
-
     private TabAdapter mTabAdapter;
     private TabLayout mTabLayout;
     private ViewPager2 mViewPager;
@@ -24,19 +26,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        mTabLayout = findViewById(R.id.tab_layout);
-        mTabAdapter = new TabAdapter(this);
-
-        mTabAdapter = new TabAdapter(this);
-
-        mViewPager = findViewById(R.id.pager);
-        mViewPager.setAdapter(mTabAdapter);
-
-        new TabLayoutMediator(mTabLayout, mViewPager,
-                (tab, position) -> tab.setText(mTabAdapter.getTabName(this, position))
-        ).attach();
+        initializeTabLayout();
     }
 
     @Override
@@ -56,5 +49,25 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    private void addTabs(TabAdapter tabAdapter) {
+        tabAdapter.addTab(getString(R.string.apps_tab_name), new AppListFragment());
+        tabAdapter.addTab(getString(R.string.queue_tab_name), new AppQueueFragment());
+        tabAdapter.addTab(getString(R.string.settings_tab_name), new SettingsFragment());
+    }
+
+    private void initializeTabLayout() {
+        mTabLayout = findViewById(R.id.tab_layout);
+        mTabAdapter = new TabAdapter(this);
+
+        addTabs(mTabAdapter);
+
+        mViewPager = findViewById(R.id.pager);
+        mViewPager.setAdapter(mTabAdapter);
+
+        new TabLayoutMediator(mTabLayout, mViewPager,
+                (tab, position) -> tab.setText(mTabAdapter.getTabName(position))
+        ).attach();
     }
 }
