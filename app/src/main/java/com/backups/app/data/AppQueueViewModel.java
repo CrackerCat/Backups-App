cbfa5bbbdcc25ab0911f2dbedf3f65d797203d45
flyingsl0ths <randomemail32110@gmail.com>
2021-01-31 00:38:27 -0600

    Added buffer to keep track of selected app count string

--- app/src/main/java/com/backups/app/data/AppQueueViewModel.java
+++ app/src/main/java/com/backups/app/data/AppQueueViewModel.java
@@ -2,29 +2,46 @@ package com.backups.app.data;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class AppQueueViewModel {
+public class AppQueueViewModel extends ViewModel {
     private final Queue<SelectedAPK> mSelectedApps;
     private final MutableLiveData<Queue<SelectedAPK>> mAppQueue;
+    private final StringBuffer mSelectedAppsBuffer;
 
     public AppQueueViewModel() {
         mSelectedApps = new LinkedList<>();
-        mAppQueue = new MutableLiveData<>();
-        mAppQueue.setValue(mSelectedApps);
+
+        mAppQueue = new MutableLiveData<>(mSelectedApps);
+
+        int initialCapacity = 3;
+        mSelectedAppsBuffer = new StringBuffer(initialCapacity);
+    }
+
+    private void updateBuffer() {
+        mSelectedAppsBuffer.delete(0, mSelectedAppsBuffer.length());
+        mSelectedAppsBuffer.append(mSelectedApps.size());
     }
 
     public void push(SelectedAPK selectedAPK) {
         mSelectedApps.add(selectedAPK);
+        updateBuffer();
     }
 
     public SelectedAPK pop() {
-        return mSelectedApps.remove();
+        SelectedAPK top = mSelectedApps.remove();
+        updateBuffer();
+        return top;
     }
 
-    public LiveData<Queue<SelectedAPK>> loadSelectedApps() {
+    public String getTotal() {
+        return (mSelectedAppsBuffer.toString());
+    }
+
+    public LiveData<Queue<SelectedAPK>> getSelectedApps() {
         return mAppQueue;
     }
 }
