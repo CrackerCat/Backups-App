f955257d3673beeb3d5199bc8125f1ca216cfced
flyingsl0ths <randomemail32110@gmail.com>
2021-02-17 02:34:11 -0600

    Removed fetchSearchResult

--- app/src/main/java/com/backups/app/data/IAPKFileRepository.java
+++ app/src/main/java/com/backups/app/data/IAPKFileRepository.java
@@ -1,10 +1,9 @@
 package com.backups.app.data;
 
 import android.content.pm.PackageManager;
-
 import java.util.List;
 
 interface IAPKFileRepository {
-   void fetchInstalledApps(final PackageManager packageManager, final Callback<List<APKFile>> callback);
-   void fetchSearchResult(final String query, final List<APKFile> apps, final Callback<List<APKFile>> callback);
+  void fetchInstalledApps(final PackageManager packageManager,
+                          final Callback<List<APKFile>> callback);
 }
