c7b5ad779e9317ebd1eb81a90c197be863ca51af
flyingsl0ths <randomemail32110@gmail.com>
2021-02-17 02:39:43 -0600

    Removed app count string buffering logic

--- app/src/main/java/com/backups/app/data/AppQueueViewModel.java
+++ app/src/main/java/com/backups/app/data/AppQueueViewModel.java
@@ -3,45 +3,25 @@ package com.backups.app.data;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class AppQueueViewModel extends ViewModel {
-    private final Queue<SelectedAPK> mSelectedApps;
-    private final MutableLiveData<Queue<SelectedAPK>> mAppQueue;
-    private final StringBuffer mSelectedAppsBuffer;
+  private final Queue<APKFile> mSelectedApps = new LinkedList<>();
+  private final MutableLiveData<Queue<APKFile>> mAppQueue =
+      new MutableLiveData<>(mSelectedApps);
 
-    public AppQueueViewModel() {
-        mSelectedApps = new LinkedList<>();
+  public void push(APKFile selectedAPK) {
+    mSelectedApps.add(selectedAPK);
+  }
 
-        mAppQueue = new MutableLiveData<>(mSelectedApps);
+  public final APKFile pop() {
+    return mSelectedApps.remove();
+  }
 
-        int initialCapacity = 3;
-        mSelectedAppsBuffer = new StringBuffer(initialCapacity);
-    }
+  public int getSelectedAppCount() {return mSelectedApps.size();}
 
-    private void updateBuffer() {
-        mSelectedAppsBuffer.delete(0, mSelectedAppsBuffer.length());
-        mSelectedAppsBuffer.append(mSelectedApps.size());
-    }
-
-    public void push(SelectedAPK selectedAPK) {
-        mSelectedApps.add(selectedAPK);
-        updateBuffer();
-    }
-
-    public SelectedAPK pop() {
-        SelectedAPK top = mSelectedApps.remove();
-        updateBuffer();
-        return top;
-    }
-
-    public String getTotal() {
-        return (mSelectedAppsBuffer.toString());
-    }
-
-    public LiveData<Queue<SelectedAPK>> getSelectedApps() {
-        return mAppQueue;
-    }
+  public final LiveData<Queue<APKFile>> getSelectedApps() {
+    return mAppQueue;
+  }
 }
