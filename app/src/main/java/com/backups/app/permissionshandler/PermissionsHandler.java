4b89f68b017c959fc8292d9290211cf3c049b59c
flyingsl0ths <randomemail32110@gmail.com>
2021-02-17 02:23:06 -0600

    Updated fileystem permission method's parameters

--- app/src/main/java/com/backups/app/permissionshandler/PermissionsHandler.java
+++ app/src/main/java/com/backups/app/permissionshandler/PermissionsHandler.java
@@ -1,25 +1,20 @@
 package com.backups.app.permissionshandler;
 
-
 import android.content.Intent;
-
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 public class PermissionsHandler {
-    public static final int CREATE_DIRECTORY_CODE = 51;
+  public static final int CREATE_DIRECTORY_CODE = 51;
 
-    protected static boolean checkExternalFilesystemPermissions() {
-        return false;
-    }
+  protected static boolean checkExternalFilesystemPermissions() {
+    return false;
+  }
 
-    protected static void promptUserForPermissions() {
+  protected static void promptUserForPermissions() {}
 
-    }
-
-    protected static void askForExternalFilesystemPermissions(AppCompatActivity activity) {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-        activity.startActivityForResult(intent, CREATE_DIRECTORY_CODE);
-
-    }
+  protected static void askForExternalFilesystemPermissions(FragmentActivity activity) {
+    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+    intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+    activity.startActivityForResult(intent, CREATE_DIRECTORY_CODE);
+  }
 }
