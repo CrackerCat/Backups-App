81856a99e0498dd56fe9fe20f8c44afcea1cf7dd
flyingsl0ths <randomemail32110@gmail.com>
2021-02-17 02:34:40 -0600

    Refactored getInstalledApps & removed interface methods impl.

--- app/src/main/java/com/backups/app/data/APKFileRepository.java
+++ app/src/main/java/com/backups/app/data/APKFileRepository.java
@@ -3,77 +3,76 @@ package com.backups.app.data;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Executor;
 
 public class APKFileRepository implements IAPKFileRepository {
 
-    private final Executor mExecutor;
-    private boolean mDisplaySystemApps = false;
+  private final Executor mExecutor;
+  private boolean mDisplaySystemApps = false;
 
-    public APKFileRepository(final Executor executor) {
-        mExecutor = executor;
-    }
+  public APKFileRepository(final Executor executor) { mExecutor = executor; }
 
-    private boolean isSystemApp(final ApplicationInfo applicationInfo) {
-        return ((applicationInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0);
-    }
+  private boolean isSystemApp(final ApplicationInfo applicationInfo) {
+    return ((applicationInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) !=
+            0);
+  }
 
-    public void displaySystemApps(boolean choice) {
-        mDisplaySystemApps = choice;
-    }
+  public void displaySystemApps(boolean choice) { mDisplaySystemApps = choice; }
 
-    private List<APKFile> getInstalledApps(final PackageManager packageManager) {
-        LinkedList<APKFile> installedApps = new LinkedList<>();
-        List<ApplicationInfo> packages = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+  private List<APKFile> getInstalledApps(final PackageManager packageManager) {
+    List<ApplicationInfo> packages =
+        packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
 
-        for (ApplicationInfo applicationInfo : packages) {
-            if (!isSystemApp(applicationInfo) && !mDisplaySystemApps || isSystemApp(applicationInfo) && mDisplaySystemApps) {
+    ArrayList<APKFile> installedApps = null;
 
-                String name = applicationInfo.loadLabel(packageManager).toString();
+    if (!packages.isEmpty()) {
+      int totalApps = packages.size();
+      installedApps = new ArrayList<>(totalApps);
 
-                String packageName;
-                if (TextUtils.containsPackageNamePrefix(name)) {
-                    packageName = name;
-                    name = TextUtils.extractHumanReadableName(name);
-                } else {
-                    packageName = applicationInfo.packageName;
-                }
+      for (ApplicationInfo applicationInfo : packages) {
+        if (!isSystemApp(applicationInfo) && !mDisplaySystemApps ||
+            isSystemApp(applicationInfo) && mDisplaySystemApps) {
 
-                String apkPath = applicationInfo.sourceDir;
-                long apkSize = new File(applicationInfo.sourceDir).length();
-                Drawable icon = packageManager.getApplicationIcon(applicationInfo);
+          String name = applicationInfo.loadLabel(packageManager).toString();
 
-                installedApps.addLast(new APKFile(name, packageName, apkPath, apkSize, icon));
-            }
+          String packageName;
+          if (PackageNameUtils.containsPackageNamePrefix(name)) {
+            packageName = name;
+            name = PackageNameUtils.extractHumanReadableName(name);
+          } else {
+            packageName = applicationInfo.packageName;
+          }
+
+          String apkPath = applicationInfo.sourceDir;
+          long apkSize = new File(applicationInfo.sourceDir).length();
+          Drawable icon = packageManager.getApplicationIcon(applicationInfo);
+
+          installedApps.add(
+              new APKFile(name, packageName, apkPath, apkSize, icon));
         }
+      }
 
-        Collections.sort(installedApps, (o1, o2) -> o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase()));
-
-        return installedApps;
+      Collections.sort(installedApps,
+                       (o1, o2)
+                           -> o1.getName().toLowerCase().compareTo(
+                               o2.getName().toLowerCase()));
     }
 
-    private List<APKFile> searchForIn(final String query, final List<APKFile> apps) {
-        return null;
-    }
+    return installedApps;
+  }
 
-    @Override
-    public void fetchInstalledApps(final PackageManager packageManager, final Callback<List<APKFile>> callback) {
-        mExecutor.execute(() -> {
-            List<APKFile> apkFiles = getInstalledApps(packageManager);
-            callback.onComplete(apkFiles);
-        });
-    }
-
-    @Override
-    public void fetchSearchResult(final String query, final List<APKFile> apps, final Callback<List<APKFile>> callback) {
-        mExecutor.execute(() -> {
-            List<APKFile> foundApps = searchForIn(query, apps);
-            callback.onComplete(foundApps);
-        });
-    }
+  @Override
+  public void fetchInstalledApps(final PackageManager packageManager,
+                                 final Callback<List<APKFile>> callback) {
+    mExecutor.execute(() -> {
+      List<APKFile> apkFiles = getInstalledApps(packageManager);
+      callback.onComplete(apkFiles);
+    });
+  }
 }
