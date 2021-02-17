75c443df7d78a4cb487df046a2f88c951859d86e
flyingsl0ths <randomemail32110@gmail.com>
2021-02-17 02:32:07 -0600

    Made return types for getters final

--- app/src/main/java/com/backups/app/data/APKFile.java
+++ app/src/main/java/com/backups/app/data/APKFile.java
@@ -3,48 +3,49 @@ package com.backups.app.data;
 import android.graphics.drawable.Drawable;
 
 public class APKFile {
-    private final String mName;
-    private final String mPackageName;
-    private final String mPackagePath;
-    private final long mAppSize;
-    private final Drawable mIcon;
+  private final String mName;
+  private final String mPackageName;
+  private final String mPackagePath;
+  private final long mAppSize;
+  private final Drawable mIcon;
 
-    APKFile(final String name, final String packageName, final String packagePath, final long appSize, final Drawable icon) {
-        mName = name;
-        mPackageName = packageName;
-        mPackagePath = packagePath;
-        mAppSize = appSize;
-        mIcon = icon;
+   APKFile(final String name, final String packageName, final String packagePath, final long appSize,
+           final Drawable icon) {
+     mName = name;
+     mPackageName = packageName;
+     mPackagePath = packagePath;
+     mAppSize = appSize;
+     mIcon = icon;
+   }
+
+    public final String getName() {
+     return mName;
     }
 
-    public String getName() {
-        return mName;
+    public final String getPackageName() {
+     return mPackageName;
     }
 
-    public String getPackageName() {
-        return mPackageName;
-    }
-
-    public String getPackagePath() {
-        return mPackagePath;
+    public final String getPackagePath() {
+     return mPackagePath;
     }
 
     public long getAppSize() {
-        return mAppSize;
+     return mAppSize;
     }
 
-    public Drawable getIcon() {
-        return mIcon;
+    public final Drawable getIcon() {
+     return mIcon;
     }
 
     @Override
     public String toString() {
-        return "ApkFile {\n" +
-                " name: " + mName +
-                ",\n packageName: " + mPackageName +
-                ",\n packagePath: " + mPackagePath +
-                ",\n appSize: " + mAppSize +
-                ",\n icon: " + mIcon +
-                "\n}";
+     return "ApkFile {\n" +
+             " name: " + mName +
+             ",\n packageName: " + mPackageName +
+             ",\n packagePath: " + mPackagePath +
+             ",\n appSize: " + mAppSize +
+             ",\n icon: " + mIcon +
+             "\n}";
     }
 }
