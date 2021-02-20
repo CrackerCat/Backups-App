35daa04c13db7ba3379636bf5d6505ce366e69bc
flyingsl0ths <randomemail32110@gmail.com>
2021-02-20 02:07:30 -0600

    Added methods for tracking repeated backup names

--- app/src/main/java/com/backups/app/data/PackageNameUtils.java
+++ app/src/main/java/com/backups/app/data/PackageNameUtils.java
@@ -1,10 +1,18 @@
 package com.backups.app.data;
 
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
+
 public class PackageNameUtils {
-  private static final String[] prefixes = {"com.", "org."};
+  private static final String[] sPrefixes = {"com.", "org."};
+  private static final String sRepeatedBackupFormatterString = "%s (%d)";
+  private static final StringBuilder sBuffer = new StringBuilder();
+  private static final Hashtable<String, Integer> sTimesTable =
+      new Hashtable<>();
 
   public static boolean containsPackageNamePrefix(final String appName) {
-    for (String prefix : prefixes) {
+    for (String prefix : sPrefixes) {
       if (appName.contains(prefix)) {
         return true;
       }
@@ -24,11 +32,38 @@ public class PackageNameUtils {
 
     char firstLetterOfName = appName.charAt(indexOfFirstLetter);
 
-    if(Character.isUpperCase(firstLetterOfName)) {
-      firstLetterOfName =
-              Character.toUpperCase(firstLetterOfName);
+    if (Character.isUpperCase(firstLetterOfName)) {
+      firstLetterOfName = Character.toUpperCase(firstLetterOfName);
     }
 
     return firstLetterOfName + appName.substring(indexAfterFirstLetter);
+  }
+
+  public void resetRepeatedCountTable() {
+    Set<Map.Entry<String, Integer>> pairs = sTimesTable.entrySet();
+
+    for (Map.Entry<String, Integer> pair : pairs) {
+      pair.setValue(0);
+    }
+  }
+
+  public static String computeRepeatedBackupName(String name) {
+    String formattedName = null;
+    if (sTimesTable.containsKey(name)) {
+      int count = sTimesTable.get(name);
+
+      int updatedCount = count + 1;
+      sTimesTable.put(name, updatedCount);
+
+      sBuffer.append(
+          String.format(sRepeatedBackupFormatterString, name, updatedCount));
+
+      formattedName = sBuffer.toString();
+      sBuffer.delete(0, formattedName.length());
+    } else {
+      int initialValue = 0;
+      sTimesTable.put(name, initialValue);
+    }
+    return formattedName;
   }
 }
