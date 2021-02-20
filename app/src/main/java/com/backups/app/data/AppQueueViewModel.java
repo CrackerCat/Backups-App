d412c64023e69d5de391012e1627f9ce6444fd1d
flyingsl0ths <randomemail32110@gmail.com>
2021-02-20 01:28:27 -0600

    Added app queue implementation

--- app/src/main/java/com/backups/app/data/AppQueueViewModel.java
+++ app/src/main/java/com/backups/app/data/AppQueueViewModel.java
@@ -3,25 +3,36 @@ package com.backups.app.data;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-import java.util.LinkedList;
-import java.util.Queue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class AppQueueViewModel extends ViewModel {
-  private final Queue<APKFile> mSelectedApps = new LinkedList<>();
-  private final MutableLiveData<Queue<APKFile>> mAppQueue =
+  private final List<APKFile> mSelectedApps = new ArrayList<>();
+  private static final HashMap<String, Integer> mTimesTable = new HashMap<>();
+
+
+  private final MutableLiveData<List<APKFile>> mAppQueue =
       new MutableLiveData<>(mSelectedApps);
 
-  public void push(APKFile selectedAPK) {
-    mSelectedApps.add(selectedAPK);
-  }
+  public void addApp(APKFile apkFile) {
+    String selectedAPKName = apkFile.getName();
 
-  public final APKFile pop() {
-    return mSelectedApps.remove();
+    String repeatedBackupName = PackageNameUtils.computeRepeatedBackupName(selectedAPKName);
+    if(repeatedBackupName != null) {
+      APKFile repeat = new APKFile(repeatedBackupName, apkFile.getPackageName(), apkFile.getPackagePath(), apkFile.getAppSize(), apkFile.getIcon());
+      mSelectedApps.add(repeat);
+    } else {
+      mSelectedApps.add(apkFile);
+    }
+
+    mAppQueue.setValue(mSelectedApps);
   }
 
   public int getSelectedAppCount() {return mSelectedApps.size();}
 
-  public final LiveData<Queue<APKFile>> getSelectedApps() {
+  public final LiveData<List<APKFile>> getAppQueue() {
     return mAppQueue;
   }
 }
