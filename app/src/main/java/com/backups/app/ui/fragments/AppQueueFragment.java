a4bfab733b6f5a9c7a993340a0d28a24ac5c14aa
flyingsl0ths <randomemail32110@gmail.com>
2021-02-21 12:19:47 -0600

    [onCreateView]: Added check to prevent recyclerview dataset change

--- app/src/main/java/com/backups/app/ui/fragments/AppQueueFragment.java
+++ app/src/main/java/com/backups/app/ui/fragments/AppQueueFragment.java
@@ -8,12 +8,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.backups.app.R;
+import com.backups.app.data.APKFile;
 import com.backups.app.data.AppQueueViewModel;
 import com.backups.app.ui.adapters.AppQueueAdapter;
+import java.util.List;
 
 public class AppQueueFragment extends Fragment {
 
@@ -43,15 +46,18 @@ public class AppQueueFragment extends Fragment {
     mAppQueueRecyclerView = view.findViewById(R.id.app_queue_rv);
 
     initializeRecyclerView(parent);
-
     mAppQueueViewModel.getAppQueue().observe(
-        getViewLifecycleOwner(),
-        appQueue -> mAppQueueAdapter.dataSetChanged(appQueue));
+        getViewLifecycleOwner(), appQueue -> {
+          if (getLifecycle().getCurrentState() != Lifecycle.State.CREATED) {
+            mAppQueueAdapter.notifyDataSetChanged();
+          }
+        });
   }
 
   private void initializeRecyclerView(FragmentActivity parent) {
     LinearLayoutManager layout = new LinearLayoutManager(parent);
-    mAppQueueAdapter = new AppQueueAdapter();
+    List<APKFile> queue = mAppQueueViewModel.getAppQueue().getValue();
+    mAppQueueAdapter = new AppQueueAdapter(queue);
     mAppQueueRecyclerView.setLayoutManager(layout);
     mAppQueueRecyclerView.setAdapter(mAppQueueAdapter);
   }
