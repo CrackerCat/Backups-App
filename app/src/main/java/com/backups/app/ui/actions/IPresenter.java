1a984d84728c67e0bc4fde409752eec2dbeca13b
flyingsl0ths <randomemail32110@gmail.com>
2021-03-09 01:16:14 -0600

    Renamed files

--- app/src/main/java/com/backups/app/ui/actions/IPresenter.java
+++ app/src/main/java/com/backups/app/ui/actions/IPresenter.java
@@ -1,15 +1,23 @@
 package com.backups.app.ui.actions;
 
-public interface IPresentor {
+public interface IPresenter {
   void addActions(IAction[] actions);
 
   void swapActions(int setId);
 
   void available(int actionID, boolean flag);
 
+  void available(int actionSet, int actionID, boolean flag);
+
+  boolean isActionAvailable(int actionID);
+
+  boolean isActionAvailable(int actionSet, int actionID);
+
   void present();
 
   void hideActions();
 
   int totalAvailableActions();
+
+  int totalAvailableActionSets();
 }
