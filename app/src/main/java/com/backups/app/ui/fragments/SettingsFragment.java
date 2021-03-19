e267489f258bba1563b5077833428924f9af401c
flyingsl0ths <randomemail32110@gmail.com>
2021-03-18 19:23:05 -0600

    Added settings tab

--- app/src/main/java/com/backups/app/ui/fragments/SettingsFragment.java
+++ app/src/main/java/com/backups/app/ui/fragments/SettingsFragment.java
@@ -1,20 +1,79 @@
 package com.backups.app.ui.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
 import com.backups.app.R;
 
-public class SettingsFragment extends Fragment {
-  @Nullable
+public class SettingsFragment extends PreferenceFragmentCompat
+    implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+  private static final int sShowSystemAppsPrefId = 1;
+  private static final int sAppThemePrefId = 2;
+
   @Override
-  public View onCreateView(@NonNull LayoutInflater inflater,
-                           @Nullable ViewGroup container,
-                           @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.settings_fragment, container, false);
+  public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    setPreferencesFromResource(R.xml.preferences, rootKey);
+
+    loadPreferences();
+
+    PreferenceManager.getDefaultSharedPreferences(requireActivity())
+        .registerOnSharedPreferenceChangeListener(this);
+  }
+
+  private void loadPreferences() {
+    PreferenceScreen preferenceScreen = getPreferenceScreen();
+    SharedPreferences sharedPreferences =
+        PreferenceManager.getDefaultSharedPreferences(requireActivity());
+
+    for (int i = 0, total = preferenceScreen.getPreferenceCount(); i < total;
+         ++i) {
+      Preference preference = preferenceScreen.getPreference(i);
+
+      if (i == sShowSystemAppsPrefId || i == sAppThemePrefId) {
+        boolean value =
+            sharedPreferences.getBoolean(preference.getKey(), false);
+        ((SwitchPreference)preference).setChecked(value);
+      }
+    }
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    PreferenceManager.getDefaultSharedPreferences(requireActivity())
+        .registerOnSharedPreferenceChangeListener(this);
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    PreferenceManager.getDefaultSharedPreferences(requireActivity())
+        .unregisterOnSharedPreferenceChangeListener(this);
+  }
+
+  @Override
+  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                        String key) {
+    Preference preference = findPreference(key);
+    if (preference != null) {
+      if (preference.getOrder() == sAppThemePrefId) {
+        boolean value = sharedPreferences.getBoolean(key, false);
+
+        String themeString;
+
+        if (!value) {
+          themeString = getResources().getString(R.string.light_theme);
+        } else {
+          themeString = getResources().getString(R.string.dark_theme);
+        }
+
+        preference.setSummary(themeString);
+      }
+    }
   }
 }
