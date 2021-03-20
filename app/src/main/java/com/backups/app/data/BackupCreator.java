8d2310c48f2ec6545f626200df9bb3188456d906
flyingsl0ths <randomemail32110@gmail.com>
2021-03-19 23:51:27 -0600

    Added stub files for classes needed for backup creation

--- app/src/main/java/com/backups/app/data/BackupCreator.java
+++ app/src/main/java/com/backups/app/data/BackupCreator.java
@@ -1,7 +1,3 @@
 package com.backups.app.data;
 
-public class BackupCreator implements IBackups {
-  private static final String OUTPUT_DIRECTORY = "Backups";
-
-  public static String getOutputDirectory() { return OUTPUT_DIRECTORY; }
-}
+public class BackupCreator implements IBackups {}
---
+++ app/src/main/java/com/backups/app/data/BackupProgress.java
@@ -0,0 +1,6 @@
+package com.backups.app.data;
+
+public class BackupProgress {
+  public String currentBackup = null;
+  public int progress = 0;
+}
