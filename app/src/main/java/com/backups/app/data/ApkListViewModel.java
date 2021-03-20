63deb807ac69b0a99cb03aa5f1c0b29e6763d587
flyingsl0ths <randomemail32110@gmail.com>
2021-03-19 23:47:04 -0600

    Reworked method for obtaining apk data

--- app/src/main/java/com/backups/app/data/ApkListViewModel.java
+++ app/src/main/java/com/backups/app/data/ApkListViewModel.java
@@ -4,28 +4,21 @@ import android.content.pm.PackageManager;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 
 public class ApkListViewModel extends ViewModel {
   private final APKFileRepository mAPKFileRepository =
       new APKFileRepository(Executors.newSingleThreadExecutor());
+
   private final MutableLiveData<List<APKFile>> mAppListMutableLiveData =
       new MutableLiveData<>();
 
   public void fetchInstalledApps(final PackageManager packageManager,
                                  boolean showSystemApps) {
-    if (mAppListMutableLiveData.getValue() == null) {
-      mAPKFileRepository.displaySystemApps(showSystemApps);
-      mAPKFileRepository.fetchInstalledApps(packageManager, result -> {
-        if (result != null) {
-          mAppListMutableLiveData.postValue(result);
-        } else {
-          mAppListMutableLiveData.postValue(new ArrayList<>());
-        }
-      });
-    }
+    mAPKFileRepository.displaySystemApps(showSystemApps);
+    mAPKFileRepository.fetchInstalledApps(packageManager,
+                                          mAppListMutableLiveData::postValue);
   }
 
   public final LiveData<List<APKFile>> getApkListLiveData() {
