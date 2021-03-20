f06dddb72761d5ebae861a5541dcbeb9bdeff31a
flyingsl0ths <randomemail32110@gmail.com>
2021-03-19 23:52:53 -0600

    Added onSharedPreferencesChanged, loadSettings, and initializeStringValues

--- app/src/main/java/com/backups/app/MainActivity.java
+++ app/src/main/java/com/backups/app/MainActivity.java
@@ -1,28 +1,21 @@
 package com.backups.app;
 
-import static com.backups.app.ui.Constants.APPLIST;
-import static com.backups.app.ui.Constants.APPQUEUE;
-import static com.backups.app.ui.Constants.BACKUP_BUTTON;
-import static com.backups.app.ui.Constants.SEARCH_BUTTON;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.documentfile.provider.DocumentFile;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceManager;
 import androidx.viewpager2.widget.ViewPager2;
+
 import com.backups.app.data.ApkListViewModel;
 import com.backups.app.data.AppQueueViewModel;
-import com.backups.app.data.BackupCreator;
-import com.backups.app.permissionshandler.PermissionsHandler;
 import com.backups.app.ui.actions.ActionPresenter;
-import com.backups.app.ui.actions.ActionSetHolder;
-import com.backups.app.ui.actions.IAction;
+import com.backups.app.ui.actions.ActionSetMaker;
 import com.backups.app.ui.adapters.TabAdapter;
 import com.backups.app.ui.fragments.AppListFragment;
 import com.backups.app.ui.fragments.AppQueueFragment;
@@ -31,36 +24,58 @@ import com.backups.app.ui.fragments.SettingsFragment;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-public class MainActivity
-    extends AppCompatActivity implements ActionPresenter.IActionAvailability,
-                                         TabLayout.OnTabSelectedListener {
+import static com.backups.app.ui.Constants.APPLIST;
+import static com.backups.app.ui.Constants.APPQUEUE;
+import static com.backups.app.ui.Constants.BACKUP_BUTTON;
+import static com.backups.app.ui.Constants.SEARCH_BUTTON;
+import static com.backups.app.ui.Constants.sAppListFragmentActionLayouts;
+import static com.backups.app.ui.Constants.sAppQueueFragmentActionLayouts;
+
+public class MainActivity extends AppCompatActivity
+    implements ActionPresenter.IActionAvailability,
+               TabLayout.OnTabSelectedListener,
+               SharedPreferences.OnSharedPreferenceChangeListener {
+  private ApkListViewModel mApkListViewModel;
   private AppQueueViewModel mAppQueueViewModel;
 
   private TextView mBackupCounterView;
   private TabAdapter mTabAdapter;
   private TabLayout mTabLayout;
   private ViewPager2 mViewPager;
-
-  private ActionSetHolder mActionSetHolder;
   private ActionPresenter mActionPresenter;
-
   private final SearchDialogFragment mAppSearchDialogFragment =
       new SearchDialogFragment();
+
+  private final static SettingsKeys sSettings = new SettingsKeys();
+  private static String sShowSystemAppsKey;
+  private static String sAppThemeKey;
+  private static String sAppTabName;
+  private static String sQueueTabName;
+  private static String sSettingsTabName;
+  private static boolean sInitializeValues = true;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    ApkListViewModel apkListViewModel =
-        new ViewModelProvider(this).get(ApkListViewModel.class);
+    initializeStringValues();
+
+    loadSettings();
+
     mAppQueueViewModel =
         new ViewModelProvider(this).get(AppQueueViewModel.class);
-    apkListViewModel.fetchInstalledApps(getPackageManager(), true);
+
+    mApkListViewModel = new ViewModelProvider(this).get(ApkListViewModel.class);
+
+    if (mApkListViewModel.getApkListLiveData().getValue() == null) {
+      mApkListViewModel.fetchInstalledApps(getPackageManager(),
+                                           sSettings.showSystemApps);
+    }
 
     initializeViews();
 
-    mAppQueueViewModel.getAppQueue().observe(this, selection -> {
+    mAppQueueViewModel.getAppQueueLiveData().observe(this, selection -> {
       if (getLifecycle().getCurrentState() != Lifecycle.State.STARTED) {
         updateBackupCountView(selection.size());
       } else {
@@ -72,33 +87,34 @@ public class MainActivity
     initializeTabLayout();
   }
 
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode,
-                                  Intent resultData) {
-    super.onActivityResult(requestCode, resultCode, resultData);
+  private void initializeStringValues() {
+    if (sInitializeValues) {
+      Resources resources = getResources();
 
-    Uri newDirectoryUri;
+      sShowSystemAppsKey = resources.getString(R.string.show_system_apps_key);
+      sAppThemeKey = resources.getString(R.string.current_theme_key);
 
-    if (resultCode == Activity.RESULT_OK) {
-      if (requestCode == PermissionsHandler.CREATE_DIRECTORY_CODE) {
-        if (resultData != null) {
-          newDirectoryUri = resultData.getData();
-          DocumentFile pickedDirectory =
-              DocumentFile.fromTreeUri(this, newDirectoryUri);
-          if (pickedDirectory != null) {
-            pickedDirectory.createDirectory(BackupCreator.getOutputDirectory());
-          }
-        }
-      }
+      sAppTabName = resources.getString(R.string.apps_tab_name);
+      sQueueTabName = resources.getString(R.string.queue_tab_name);
+      sSettingsTabName = resources.getString(R.string.settings_tab_name);
+
+      sInitializeValues = false;
     }
   }
 
+  private void loadSettings() {
+    SharedPreferences sharedPreferences =
+        PreferenceManager.getDefaultSharedPreferences(this);
+
+    sSettings.showSystemApps =
+        sharedPreferences.getBoolean(sShowSystemAppsKey, false);
+    sSettings.useDarkTheme = sharedPreferences.getBoolean(sAppThemeKey, false);
+  }
+
   private void addTabs(TabAdapter tabAdapter) {
-    tabAdapter.addTab(getString(R.string.apps_tab_name), new AppListFragment());
-    tabAdapter.addTab(getString(R.string.queue_tab_name),
-                      new AppQueueFragment());
-    tabAdapter.addTab(getString(R.string.settings_tab_name),
-                      new SettingsFragment());
+    tabAdapter.addTab(sAppTabName, new AppListFragment());
+    tabAdapter.addTab(sQueueTabName, new AppQueueFragment());
+    tabAdapter.addTab(sSettingsTabName, new SettingsFragment());
   }
 
   private void initializeViews() {
@@ -122,78 +138,69 @@ public class MainActivity
         .attach();
   }
 
-  private void initializeAppListFragmentActions() {
-    IAction[] actionButtons = mActionSetHolder.getActionSet(APPLIST);
-
-    for (int action = 0, total = actionButtons.length; action < total;
-         ++action) {
-      if (action == SEARCH_BUTTON) {
-        IAction searchButton = actionButtons[action];
-        searchButton.assignActiveCallback(v -> {
-          mAppSearchDialogFragment.setDataSetID(APPLIST);
-          mAppSearchDialogFragment.show(
-              getSupportFragmentManager(),
-              (mAppSearchDialogFragment.getClass().getSimpleName()));
-        });
-        searchButton.assignInactiveCallback(
+  private ActionSetMaker.CallBackSetup initializeAppListFragmentActions() {
+    return (position, action) -> {
+      if (position == SEARCH_BUTTON) {
+        action.assignCallBacks(
+            v
+            -> {
+              mAppSearchDialogFragment.setDataSetID(APPLIST);
+              mAppSearchDialogFragment.show(
+                  getSupportFragmentManager(),
+                  (mAppSearchDialogFragment.getClass().getSimpleName()));
+            },
             v
             -> Toast
-                   .makeText(this, R.string.fetching_data_message,
+                   .makeText(MainActivity.this, R.string.fetching_data_message,
                              Toast.LENGTH_SHORT)
                    .show());
       }
-    }
+    };
   }
 
-  private void initializeAppQueueFragmentActions() {
-    IAction[] actionButtons = mActionSetHolder.getActionSet(APPQUEUE);
-
-    for (int action = 0, total = actionButtons.length; action < total;
-         ++action) {
-      if (action == SEARCH_BUTTON) {
-        IAction searchButton = actionButtons[action];
-        searchButton.assignActiveCallback(v -> {
-          mAppSearchDialogFragment.setDataSetID(APPQUEUE);
-          mAppSearchDialogFragment.show(
-              getSupportFragmentManager(),
-              (mAppSearchDialogFragment.getClass().getSimpleName()));
-        });
-        searchButton.assignInactiveCallback(
+  private ActionSetMaker.CallBackSetup initializeAppQueueFragmentActions() {
+    return (position, action) -> {
+      if (position == SEARCH_BUTTON) {
+        action.assignCallBacks(
+            v
+            -> {
+              mAppSearchDialogFragment.setDataSetID(APPQUEUE);
+              mAppSearchDialogFragment.show(
+                  getSupportFragmentManager(),
+                  (mAppSearchDialogFragment.getClass().getSimpleName()));
+            },
             v
             -> Toast
-                   .makeText(this, R.string.fetching_data_message,
+                   .makeText(MainActivity.this, R.string.fetching_data_message,
                              Toast.LENGTH_SHORT)
                    .show());
-      } else if (action == BACKUP_BUTTON) {
-        IAction backupButton = actionButtons[action];
-        backupButton.assignActiveCallback(
+      } else if (position == BACKUP_BUTTON) {
+        action.assignCallBacks(
             v
             -> Toast
-                   .makeText(this, R.string.fetching_data_message,
+                   .makeText(MainActivity.this, R.string.fetching_data_message,
                              Toast.LENGTH_SHORT)
-                   .show());
-
-        backupButton.assignInactiveCallback(
+                   .show(),
             v
             -> Toast
-                   .makeText(this, R.string.fetching_data_message,
+                   .makeText(MainActivity.this, R.string.fetching_data_message,
                              Toast.LENGTH_SHORT)
                    .show());
       }
-    }
+    };
   }
 
   private void initializeFAButton() {
     mActionPresenter =
         new ActionPresenter(this, R.id.main_floating_action_button);
 
-    mActionSetHolder = new ActionSetHolder(mActionPresenter, this);
+    mActionPresenter.addActions(ActionSetMaker.makeActionSet(
+        mActionPresenter, this, sAppListFragmentActionLayouts,
+        initializeAppListFragmentActions()));
 
-    initializeAppListFragmentActions();
-    initializeAppQueueFragmentActions();
-
-    mActionPresenter.addActions(mActionSetHolder.getActionSet(APPLIST));
-    mActionPresenter.addActions(mActionSetHolder.getActionSet(APPQUEUE));
+    mActionPresenter.addActions(ActionSetMaker.makeActionSet(
+        mActionPresenter, this, sAppQueueFragmentActionLayouts,
+        initializeAppQueueFragmentActions()));
 
     mActionPresenter.swapActions(APPLIST);
     mActionPresenter.present();
@@ -237,6 +244,7 @@ public class MainActivity
       mActionPresenter.swapActions(position);
     } else if (position == APPQUEUE) {
       mActionPresenter.swapActions(position);
+
       if (!mActionPresenter.isActionAvailable(SEARCH_BUTTON)) {
         if (mAppQueueViewModel.hasBackups()) {
           mActionPresenter.available(SEARCH_BUTTON, true);
@@ -250,4 +258,30 @@ public class MainActivity
 
   @Override
   public void onTabReselected(TabLayout.Tab tab) {}
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    PreferenceManager.getDefaultSharedPreferences(this)
+        .registerOnSharedPreferenceChangeListener(this);
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    PreferenceManager.getDefaultSharedPreferences(this)
+        .unregisterOnSharedPreferenceChangeListener(this);
+  }
+
+  @Override
+  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                        String key) {
+    if (key.equals(sShowSystemAppsKey)) {
+      boolean choice = sharedPreferences.getBoolean(key, false);
+      sSettings.showSystemApps = choice;
+
+      mActionPresenter.available(APPLIST, SEARCH_BUTTON, false);
+      mApkListViewModel.fetchInstalledApps(getPackageManager(), choice);
+    }
+  }
 }
