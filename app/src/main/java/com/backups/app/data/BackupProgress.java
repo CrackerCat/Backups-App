d1cb7eab2a769ec62b03bdbfdd4cf908eb3ef1f9
flyingsl0ths <randomemail32110@gmail.com>
2021-04-18 22:41:03 -0500

    Implemented apk file backup creation

--- app/src/main/java/com/backups/app/data/BackupProgress.java
+++ app/src/main/java/com/backups/app/data/BackupProgress.java
@@ -1,6 +1,9 @@
 package com.backups.app.data;
 
 public class BackupProgress {
-  public String currentBackup = null;
+  public enum ProgressState { NONE, ONGOING, FINISHED, ERROR }
+
+  public ProgressState state = ProgressState.NONE;
+  public String backupName;
   public int progress = 0;
 }
--- app/src/main/java/com/backups/app/ui/fragments/AppQueueFragment.java
+++ app/src/main/java/com/backups/app/ui/fragments/AppQueueFragment.java
@@ -1,9 +1,12 @@
 package com.backups.app.ui.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -12,14 +15,24 @@ import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.backups.app.R;
 import com.backups.app.data.APKFile;
 import com.backups.app.data.AppQueueViewModel;
+import com.backups.app.data.BackupProgress;
+import com.backups.app.data.BackupsViewModelFactory;
+import com.backups.app.ui.actions.ActionPresenter;
 import com.backups.app.ui.adapters.AppQueueAdapter;
+
 import java.util.List;
+
+import static com.backups.app.ui.Constants.APPQUEUE;
+import static com.backups.app.ui.Constants.BACKUP_BUTTON;
+import static com.backups.app.ui.Constants.SEARCH_BUTTON;
 
 public class AppQueueFragment extends Fragment {
 
+  private ActionPresenter.IActionAvailability mActionNotifier;
   private AppQueueViewModel mAppQueueViewModel;
 
   private RecyclerView mAppQueueRecyclerView;
@@ -41,7 +54,8 @@ public class AppQueueFragment extends Fragment {
     FragmentActivity parent = requireActivity();
 
     mAppQueueViewModel =
-        new ViewModelProvider(parent).get(AppQueueViewModel.class);
+        new ViewModelProvider(parent, new BackupsViewModelFactory(parent))
+            .get(AppQueueViewModel.class);
 
     mAppQueueRecyclerView = view.findViewById(R.id.app_queue_rv);
 
@@ -50,16 +64,67 @@ public class AppQueueFragment extends Fragment {
     mAppQueueViewModel.getAppQueueLiveData().observe(
         getViewLifecycleOwner(), appQueue -> {
           if (getLifecycle().getCurrentState() != Lifecycle.State.CREATED) {
-            mAppQueueAdapter.notifyDataSetChanged();
+            mAppQueueAdapter.addedItem();
           }
         });
+
+    mAppQueueViewModel.getBackupProgressLiveData().observe(
+        getViewLifecycleOwner(), this::handleBackupProgress);
   }
 
   private void initializeRecyclerView(FragmentActivity parent) {
     LinearLayoutManager layout = new LinearLayoutManager(parent);
+
     List<APKFile> queue = mAppQueueViewModel.getSelectedApps();
+
     mAppQueueAdapter = new AppQueueAdapter(queue);
+
     mAppQueueRecyclerView.setLayoutManager(layout);
+
     mAppQueueRecyclerView.setAdapter(mAppQueueAdapter);
+  }
+
+  private void handleBackupProgress(BackupProgress progress) {
+    int position = 0;
+    BackupProgress.ProgressState progressState = progress.state;
+
+    boolean updateRecyclerView =
+        progressState == BackupProgress.ProgressState.FINISHED ||
+        progressState == BackupProgress.ProgressState.ERROR;
+
+    if (updateRecyclerView) {
+      mAppQueueAdapter.removedItem();
+
+      if (!mAppQueueViewModel.hasBackups()) {
+        mActionNotifier.makeActionAvailable(APPQUEUE, SEARCH_BUTTON, false);
+        mActionNotifier.makeActionAvailable(APPQUEUE, BACKUP_BUTTON, false);
+      }
+
+    } else {
+      View toUpdate = mAppQueueRecyclerView.getChildAt(position);
+
+      ProgressBar progressBar = toUpdate.findViewById(R.id.app_queue_item_pb);
+
+      progressBar.incrementProgressBy(progress.progress);
+    }
+  }
+
+  @Override
+  public void onAttach(@NonNull Context context) {
+    super.onAttach(context);
+
+    if (context instanceof ActionPresenter.IActionAvailability) {
+      mActionNotifier = (ActionPresenter.IActionAvailability)context;
+    } else {
+      String listenerCastErrorMessage =
+          "[AppQueueFragment]: Unable to cast to required class";
+      throw new ClassCastException(listenerCastErrorMessage);
+    }
+  }
+
+  @Override
+  public void onDestroy() {
+    mActionNotifier = null;
+    super.onDestroy();
   }
 }
