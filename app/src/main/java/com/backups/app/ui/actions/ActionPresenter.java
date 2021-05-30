92f5fb34256e96c2525aab0cf584628a725bfae1
flyingsl0ths <randomemail32110@gmail.com>
2021-05-30 18:28:04 -0500

    Added final qualifiers to various method parameters

--- app/src/main/java/com/backups/app/ui/actions/ActionPresenter.java
+++ app/src/main/java/com/backups/app/ui/actions/ActionPresenter.java
@@ -3,26 +3,18 @@ package com.backups.app.ui.actions;
 import androidx.annotation.IdRes;
 import androidx.fragment.app.FragmentActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public final class ActionPresenter implements IPresenter {
 
-  private final FloatingActionButton mParentButton;
-  private final HashMap<Integer, IAction[]> mActionSets = new HashMap<>();
   private boolean mIsPresenting = false;
   private boolean mIsShowingActions = false;
   private IAction[] mCurrentActions = null;
+  private final FloatingActionButton mParentButton;
+  private final ArrayList<IAction[]> mActionSets = new ArrayList<>();
 
   public ActionPresenter(final FragmentActivity view, final @IdRes int uiID) {
     mParentButton = view.findViewById(uiID);
-  }
-
-  public interface IActionAvailability {
-    int totalAvailableActions();
-    int totalAvailableActionSets();
-
-    void makeActionAvailable(int actionID, boolean flag);
-    void makeActionAvailable(int actionSet, int actionID, boolean flag);
   }
 
   @Override
@@ -33,41 +25,48 @@ public final class ActionPresenter implements IPresenter {
           return;
         }
       }
-      mActionSets.put((mActionSets.size()), actions);
+      mActionSets.add(actions);
     }
   }
 
   @Override
-  public void swapActions(int setId) {
+  public void swapActions(final int setId) {
     hideActions();
-    mCurrentActions = mActionSets.get(setId);
+
+    if (setId == IPresenter.NO_ACTIONS) {
+      mCurrentActions = null;
+    } else {
+      mCurrentActions = mActionSets.get(setId);
+    }
   }
 
   @Override
-  public void available(int action, boolean flag) {
-    boolean canBeMadeAvailable = action >= 0 && mCurrentActions != null &&
+  public void available(final int action, final boolean flag) {
+    boolean canBeMadeAvailable = mCurrentActions != null && action >= 0 &&
                                  action < mCurrentActions.length;
     if (canBeMadeAvailable) {
       if (!mCurrentActions[action].getAvailability()) {
-        (mCurrentActions[action]).availability(flag);
+        (mCurrentActions[action]).setAvailability(flag);
       }
     }
   }
 
   @Override
-  public void available(int actionSet, int actionID, boolean flag) {
+  public void available(final int actionSet, final int actionID,
+                        final boolean flag) {
     IAction[] set = mActionSets.get(actionSet);
+
     if (set != null) {
       boolean canBeMadeAvailable = actionID >= 0 && actionID < set.length;
 
       if (canBeMadeAvailable) {
-        set[actionID].availability(flag);
+        set[actionID].setAvailability(flag);
       }
     }
   }
 
   @Override
-  public boolean isActionAvailable(int actionID) {
+  public boolean isActionAvailable(final int actionID) {
     boolean exists = actionID >= 0 && mCurrentActions != null &&
                      actionID < mCurrentActions.length;
 
@@ -75,11 +74,11 @@ public final class ActionPresenter implements IPresenter {
       return mCurrentActions[actionID].getAvailability();
     }
 
-    return exists;
+    return false;
   }
 
   @Override
-  public boolean isActionAvailable(int actionSet, int actionID) {
+  public boolean isActionAvailable(final int actionSet, final int actionID) {
     IAction[] set = mActionSets.get(actionSet);
     if (set != null) {
       boolean exists = actionID >= 0 && actionID < set.length;
@@ -97,15 +96,17 @@ public final class ActionPresenter implements IPresenter {
       mIsPresenting = true;
 
       mParentButton.setOnClickListener(view -> {
-        if (!mIsShowingActions) {
-          mIsShowingActions = true;
-          for (IAction action : mCurrentActions) {
-            action.display(mIsShowingActions);
-          }
-        } else {
-          mIsShowingActions = false;
-          for (IAction action : mCurrentActions) {
-            action.display(mIsShowingActions);
+        if (mCurrentActions != null) {
+          if (!mIsShowingActions) {
+            mIsShowingActions = true;
+            for (IAction action : mCurrentActions) {
+              action.display(mIsShowingActions);
+            }
+          } else {
+            mIsShowingActions = false;
+            for (IAction action : mCurrentActions) {
+              action.display(mIsShowingActions);
+            }
           }
         }
       });
