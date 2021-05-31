73a517df3e62bc909b8a0c09fa501da6f7956b12
flyingsl0ths <randomemail32110@gmail.com>
2021-05-30 21:33:36 -0500

    Modified observer methods

--- app/src/main/java/com/backups/app/ui/fragments/SearchDialogFragment.java
+++ app/src/main/java/com/backups/app/ui/fragments/SearchDialogFragment.java
@@ -14,29 +14,27 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.backups.app.R;
-
-import com.backups.app.data.APKFile;
+import com.backups.app.data.pojos.APKFile;
 import com.backups.app.data.viewmodels.ApkListViewModel;
 import com.backups.app.data.viewmodels.AppQueueViewModel;
 import com.backups.app.data.viewmodels.BackupsViewModelFactory;
 import com.backups.app.ui.adapters.ItemClickListener;
 import com.backups.app.ui.adapters.SearchAdapter;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-
 import java.util.List;
 
 public class SearchDialogFragment
     extends DialogFragment implements ItemClickListener {
+  public enum DataSet { APP_LIST, APP_QUEUE }
+
+  private DataSet mDataSetChoice = DataSet.APP_LIST;
+
   private AppQueueViewModel mAppQueueViewModel;
   private ApkListViewModel mApkListViewModel;
 
   private SearchAdapter mSearchAdapter;
   private RecyclerView mSearchResultsRecyclerView;
   private SearchView mSearchView;
-
-  public enum DataSet { APP_LIST, APP_QUEUE }
-
-  private DataSet mDataSetChoice = DataSet.APP_LIST;
 
   @NonNull
   @Override
@@ -51,10 +49,15 @@ public class SearchDialogFragment
             .get(AppQueueViewModel.class);
 
     MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(parent);
+
     View dialogLayout = inflater.inflate(R.layout.search_dialog_fragment, null);
+
     initializeViews(dialogLayout);
+
     setupRecyclerView(parent);
+
     initializeSearchView();
+
     builder.setView(dialogLayout);
 
     return builder.create();
@@ -70,7 +73,7 @@ public class SearchDialogFragment
     if (choice.equals(DataSet.APP_LIST)) {
       dataSet = mApkListViewModel.getApkListLiveData().getValue();
     } else if (choice.equals(DataSet.APP_QUEUE)) {
-      dataSet = mAppQueueViewModel.getAppQueueLiveData().getValue();
+      dataSet = mAppQueueViewModel.getAppsInQueue();
     }
     return dataSet;
   }
@@ -79,15 +82,19 @@ public class SearchDialogFragment
     List<APKFile> data = useDataSet(mDataSetChoice);
 
     mSearchAdapter = new SearchAdapter(data);
+
     mSearchAdapter.setClickListener(this);
 
     LinearLayoutManager layoutManager = new LinearLayoutManager(activity);
+
     mSearchResultsRecyclerView.setLayoutManager(layoutManager);
+
     mSearchResultsRecyclerView.setAdapter(mSearchAdapter);
   }
 
   private void initializeSearchView() {
     mSearchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
     mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
       @Override
       public boolean onQueryTextSubmit(String query) {
@@ -104,8 +111,11 @@ public class SearchDialogFragment
 
   @Override
   public void onItemClick(View view, int position) {
-    APKFile selected = mSearchAdapter.getItem(position);
-    mAppQueueViewModel.addApp(selected);
+    if (!mAppQueueViewModel.isBackupInProgress()) {
+      APKFile selected = mSearchAdapter.getItem(position);
+
+      mAppQueueViewModel.addApp(selected);
+    }
   }
 
   public void setDataSetID(DataSet dataSetID) { mDataSetChoice = dataSetID; }
